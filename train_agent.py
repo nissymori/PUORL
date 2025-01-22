2b5ce9b0476e231cd3accfaaa53d261ba4eeff03
nissymori <kyoudai.630@gmail.com>
2025-01-23 02:48:04 +0900

    log normalized return

--- train_agent.py
+++ train_agent.py
@@ -17,6 +17,7 @@ from offlinerl import make_agent, make_evaluation
 from utils import (OfflineRLConfig, make_agent_params_path, make_classifier,
                    make_classifier_params_path, make_offline_rl_dataset,
                    make_shifted_dataset_path)
+import math
 
 """
 Dataset
@@ -145,7 +146,7 @@ def train(config: OfflineRLConfig):
         train_state, loss = train_vj(train_state, dataset, rngs, algo_config)
         if step % eval_interval == 0:
             eval_return = eval_fn(train_state)
-            normalized_eval_return = [float(positive_data_env.get_normalized_score(eval_return[i])) * 100 for i in range(config.n_seeds)]
+            normalized_eval_return = [math.floor(positive_data_env.get_normalized_score(eval_return[i]) * 100) for i in range(config.n_seeds)]
             eval_returns.append(normalized_eval_return)
             print(f"step: {step}, eval_return: {eval_return}, normalized_eval_return: {normalized_eval_return}")
     
